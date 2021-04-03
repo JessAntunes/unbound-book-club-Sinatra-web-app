eeedfae1e4047d2ac6648ed85f388ce2f7a81861
Jessica Antunes <jessantunes@protonmail.com>
2021-04-03 16:30:55 -0400

    create libraries table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210403202033) do
+ActiveRecord::Schema.define(version: 20210403202728) do
+
+  create_table "libraries", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
