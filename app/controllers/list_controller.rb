48d028d45203d4897a9968fc2a7b86b37684e98d
Jessica Antunes <jessantunes@protonmail.com>
2021-04-04 15:06:42 -0400

    add controllers to ListController

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -2,9 +2,25 @@ require './config/environment'
 
 class ListController < Sinatra::Base
 
-#   get "/:list" do
-#     @list = find_by(params[:list])
-#     erb :list
-#   end
+    get '/lists' do
+        @lists = List.all
+        erb :"lists/index.html"
+    end
+
+    get '/lists/:id' do   #:id - route variable (not literal)
+        @list = List.find(params[:id])
+        erb :"lists/show.html"   #renders post/show template in from view folder
+    end
+
+    get '/lists/new' do    #route url
+        erb :"posts/new.html"
+    end
+
+    post '/lists' do
+        @list = List.new
+        @list.title = params[:title]
+        @list.save
+        redirect "/lists/#{@post.id}" #renders view of specific list
+    end
 
 end
