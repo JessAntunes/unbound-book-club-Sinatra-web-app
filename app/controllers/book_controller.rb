7dbe23495f7d8fb052e25c660e4e3b2741a5f24d
Jessica Antunes <jessantunes@protonmail.com>
2021-04-04 18:45:47 -0400

    add post controller for book

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -2,9 +2,14 @@ require './config/environment'
 
 class BookController < Sinatra::Base
 
-  get "/newbook" do
-    @book = Book.new(params[:title])
-    erb :book
-  end
+    get "/newbook" do
+        @book = Book.new(params[:title])
+        erb :book
+    end
+
+    post "/newbook"
+        @book = Book.new(params[:title])
+        redirect "/book/:id"
+    end
 
 end
