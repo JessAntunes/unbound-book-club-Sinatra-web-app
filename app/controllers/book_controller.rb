9218a6a36fa9e76f075ba1435ccdd8e78c820bb5
Jessica Antunes <jessantunes@protonmail.com>
2021-04-05 10:21:54 -0400

    uadd version to activerecord gem

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -3,13 +3,12 @@ require './config/environment'
 class BookController < Sinatra::Base
 
     get "/book/new" do
-        @book = Book.new(params[:title])
-        erb :new_book
+        erb :"/views/new_book"
     end
 
     post "/book/:id"
         @book = Book.new(params[:title])
-        erb "/book/:id"
+        erb :'/view/book'
     end
 
 end
