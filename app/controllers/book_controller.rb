12d536d8316cec7fd0b7a78fc66186dc8aef8f4f
Jessica Antunes <jessantunes@protonmail.com>
2021-04-05 10:14:23 -0400

    remove excess code/ add comments

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -2,12 +2,12 @@ require './config/environment'
 
 class BookController < Sinatra::Base
 
-    get "/newbook" do
+    get "/book/new" do
         @book = Book.new(params[:title])
         erb :new_book
     end
 
-    post "/newbook"
+    post "/book/:id"
         @book = Book.new(params[:title])
         erb "/book/:id"
     end
