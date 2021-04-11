b28b82c821c9604d01298bbd93b642c698cdf47b
Jessica Antunes <jessantunes@protonmail.com>
2021-04-10 23:17:19 -0400

    create books instance method to show a lists books

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,13 @@ class List < ActiveRecord::Base
    belongs_to :user
    belongs_to :book
 
+   def books(user_id, list_name) 
+      List.all.map do |l|
+         if l.user_id == user_id && l.name == list_name
+            Book.find(l.book_id)
+         end
+      end
+   end
+
 
 end
